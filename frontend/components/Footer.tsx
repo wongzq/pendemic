93577395583a1887cbe11f0647d25f1eb887f8a5
WONG ZHENG QUAN <38060298+wongzhengquan@users.noreply.github.com>
2021-05-23 12:46:21 +0800

    frontend: Removed boilerplate template

--- frontend/components/Footer.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-type FooterProps = {};
-
-const Footer: React.FC<FooterProps> = () => {
-  return <div>Copyright 2021 Ninja List</div>;
-};
-export default Footer;
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,20 +1,11 @@
-import classNames from "classnames";
-import Link from "next/link";
-import Layout from "../components/Layout";
-import styles from "../styles/index.module.scss";
+import React from 'react';
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-    <div className={classNames(styles.testing, styles.testing__test)}>
-      Testing CSS Modules
-    </div>
-  </Layout>
-);
+type indexProps = {
+  
+};
 
-export default IndexPage;
+const index:React.FC<indexProps> = () => {
+  
+  return <div>Have a good coding</div>
+}
+export default index;
