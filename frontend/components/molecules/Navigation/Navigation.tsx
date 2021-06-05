4cdefd8d5f7ef9b4e552fadf9dd743b6299cf5bf
WONG ZHENG QUAN <38060298+wongzhengquan@users.noreply.github.com>
2021-06-06 02:43:23 +0800

    frontend: Updated Nav layout

--- frontend/components/molecules/Navigation/Navigation.tsx
+++ frontend/components/molecules/Navigation/Navigation.tsx
@@ -1,19 +1,19 @@
 import ImgIcon, { ImgIcons } from "@components/atoms/ImgIcon/ImgIcon";
 import React from "react";
 import styles from "./Navigation.module.scss";
-import Text from "@components/styled/Text.styled";
-import Layout from "@components/styled/Layout.styled";
+import Text from "@styled/Text.styled";
+import Layout from "@styled/Layout.styled";
 import Link from "next/link";
 import NextRoutes from "@routes/next.routes";
 import Hoverable from "@components/atoms/Hoverable/Hoverable";
 
 type NavOptionProps = {
+  color: "lavender" | "ember";
   route: string;
   children: string;
-  color: "lavender" | "ember";
 };
 
-const NavOption: React.FC<NavOptionProps> = ({ route, children, color }) => {
+const NavOption: React.FC<NavOptionProps> = ({ color, route, children }) => {
   return (
     <div className={styles[`nav_option--${color}`]}>
       <Hoverable.Underline color={color} height={4}>
@@ -37,6 +37,20 @@ const Navigation: React.FC<NavigationProps> = () => {
   return (
     <Layout.Centered>
       <nav className={styles.main}>
+        {loggedIn ? (
+          <div className={styles.nav_1}>
+            <NavOption route={NextRoutes.plan} color="lavender">
+              Plan
+            </NavOption>
+          </div>
+        ) : (
+          <div className={styles.nav_1}>
+            <NavOption route={NextRoutes.signup} color="lavender">
+              Sign Up
+            </NavOption>
+          </div>
+        )}
+
         <Link href={NextRoutes.home}>
           <div className={styles.logo}>
             <Hoverable.Fade
@@ -55,19 +69,13 @@ const Navigation: React.FC<NavigationProps> = () => {
         </Link>
 
         {loggedIn ? (
-          <div className={styles.nav}>
-            <NavOption route={NextRoutes.plan} color="lavender">
-              Plan
-            </NavOption>
+          <div className={styles.nav_2}>
             <NavOption route={NextRoutes.write} color="ember">
               Write
             </NavOption>
           </div>
         ) : (
-          <div className={styles.nav}>
-            <NavOption route={NextRoutes.login} color="lavender">
-              Sign Up
-            </NavOption>
+          <div className={styles.nav_2}>
             <NavOption route={NextRoutes.login} color="ember">
               Login
             </NavOption>
