9a46011e4f4db928e3eae68a572fb52b68f8d762
WONG ZHENG QUAN <38060298+wongzhengquan@users.noreply.github.com>
2021-06-13 00:13:39 +0800

    app: Updated useDimensions to use useEffect and updated scrollbar css

--- app/frontend/hooks/dimensions.hook.tsx
+++ app/frontend/hooks/dimensions.hook.tsx
@@ -1,6 +1,13 @@
+import React from "react";
 import { useMediaQuery } from "react-responsive";
 
 const useDimensions = () => {
+  const [is320, setIs320] = React.useState(false);
+  const [is480, setIs480] = React.useState(false);
+  const [is768, setIs768] = React.useState(false);
+  const [is1024, setIs1024] = React.useState(false);
+  const [is1200, setIs1200] = React.useState(false);
+
   const is320Browser = useMediaQuery({ minWidth: 320 });
   const is480Browser = useMediaQuery({ minWidth: 480 });
   const is768Browser = useMediaQuery({ minWidth: 768 });
@@ -13,26 +20,20 @@ const useDimensions = () => {
   const is1024Device = useMediaQuery({ minDeviceWidth: 1024 });
   const is1280Device = useMediaQuery({ minDeviceWidth: 1280 });
 
-  const is320 = is320Browser || is320Device;
-  const is480 = is480Browser || is480Device;
-  const is768 = is768Browser || is768Device;
-  const is1024 = is1024Browser || is1024Device;
-  const is1200 = is1280Browser || is1280Device;
-
-  const isMini = !is320;
-  const isMobile = is320 && !is480;
-  const isTabletPortrait = is480 && !is768;
-  const isTabletLandscape = is768 && !is1024;
-  const isLaptop = is1024 && !is1200;
-  const isDesktop = is1200;
+  React.useEffect(() => {
+    setIs320(is320Browser || is320Device);
+    setIs480(is480Browser || is480Device);
+    setIs768(is768Browser || is768Device);
+    setIs1024(is1024Browser || is1024Device);
+    setIs1200(is1280Browser || is1280Device);
+  });
 
   return {
-    isMini,
-    isMobile,
-    isTabletPortrait,
-    isTabletLandscape,
-    isLaptop,
-    isDesktop,
+    is320,
+    is480,
+    is768,
+    is1024,
+    is1200,
   };
 };
 
