169b2ffa5747881bd6fc1d242bf82ef5da873019
WONG ZHENG QUAN <38060298+wongzhengquan@users.noreply.github.com>
2021-06-13 07:32:33 +0800

    app: Updated sequelize init format

--- app/backend/models/cleardb/index.ts
+++ app/backend/models/cleardb/index.ts
@@ -2,6 +2,15 @@ import ServerConfig from "#configs/server.config";
 import { initModels } from "#models/cleardb/init-models";
 import { Sequelize } from "sequelize";
 
-const Models = initModels(new Sequelize(ServerConfig.DB_URI));
+const Models = initModels(
+  new Sequelize({
+    host: ServerConfig.DB_HOST,
+    username: ServerConfig.DB_USER,
+    password: ServerConfig.DB_PSWD,
+    database: ServerConfig.DB_NAME,
+    dialect: "mysql",
+    dialectModule: require("mysql2"),
+  })
+);
 
 export default Models;
